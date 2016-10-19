08f88d62cc329472b3a45188b94228d44c5826df
hwjwill <hwjwill@hotmail>
2016-10-19 03:30:14 -0700

    still has major bug

--- main.m
+++ main.m
@@ -1,11 +1,30 @@
-function [] = main(imname1, points1, points2)
+function [] = main(imname1, points1csv, points2csv)
+points1 = csvread(points1csv);
+points2 = csvread(points2csv);
 im1 = imread(imname1);
+im1 = rgb2gray(im1);
+figure(1);
 imshow(im1);
+
 points = (points1 + points2) / 2;
+%points = points1 * 0.9 + points2 * 0.1;
 tri = delaunay(points);
 [midR, midC] = splitRC(tri, points);
 [r1, c1] = splitRC(tri, points1);
-A = computeAffine([r1(1, :), c1(1, :)], [midR(1, :), midC(1, :)])
+
+result = uint8(zeros(size(im1)));
+for i = 1:3
+    A = computeAffine([r1(i, :), c1(i, :)], [midR(i, :), midC(i, :)]);
+    mask = roipoly(im1, r1(i, :), c1(i, :));
+    [triR, triC] = find(mask);
+    x = [triR'; triC'; ones(size(triR))'];
+    y = round(A * x);
+    for j = 1:size(y, 2)
+        result(y(1, j), y(2, j)) = im1(x(1, j), x(2, j));
+    end
+end
+figure(2);
+imshow(result);
 hold on;
 for a = 1:size(midR, 1)
     line([midR(a, 1), midR(a, 2)], [midC(a, 1), midC(a, 2)]);

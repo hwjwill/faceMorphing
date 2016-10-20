69e55b4eea204e356fe4c63158c57202618709a3
Will Huang <hwjwill@berkeley.edu>
2016-10-19 23:07:37 -0700

    Fixed bug

--- main.m
+++ main.m
@@ -1,17 +1,35 @@
-function [] = main(imname1, points1csv, points2csv)
+function [] = main(imname1, imname2, points1csv, points2csv)
 points1 = csvread(points1csv);
 points2 = csvread(points2csv);
 im1 = imread(imname1);
+im2 = imread(imname2);
 im1 = rgb2gray(im1);
-figure(1);
-imshow(im1);
+im2 = rgb2gray(im2);
 
 points = (points1 + points2) / 2;
 %points = points1 * 0.9 + points2 * 0.1;
 tri = delaunay(points);
 [midR, midC] = splitRC(tri, points);
 [r1, c1] = splitRC(tri, points1);
+[r2, c2] = splitRC(tri, points2);
 
+result1 = warp(im1, r1, c1, midR, midC);
+result2 = warp(im2, r2, c2, midR, midC);
+figure(1);
+imshow(result1);
+result = result1 * 0.5 + result2 * 0.5;
+figure(2);
+imshow(result);
+% hold on;
+% for a = 1:size(midR, 1)
+%     line([midC(a, 1), midC(a, 2)], [midR(a, 1), midR(a, 2)]);
+%     line([midC(a, 3), midC(a, 2)], [midR(a, 3), midR(a, 2)]);
+%     line([midC(a, 1), midC(a, 3)], [midR(a, 1), midR(a, 3)]);
+% end
+% hold off;
+end
+
+function [result] = warp(im1, r1, c1, midR, midC)
 result = uint8(zeros(size(im1)));
 for i = 1:size(midR, 1)
     A = computeAffine([r1(i, :), c1(i, :)], [midR(i, :), midC(i, :)]);
@@ -23,15 +41,6 @@ for i = 1:size(midR, 1)
         result(y(1, j), y(2, j)) = im1(x(1, j), x(2, j));
     end
 end
-figure(2);
-imshow(result);
-hold on;
-for a = 1:size(midR, 1)
-    line([midC(a, 1), midC(a, 2)], [midR(a, 1), midR(a, 2)]);
-    line([midC(a, 3), midC(a, 2)], [midR(a, 3), midR(a, 2)]);
-    line([midC(a, 1), midC(a, 3)], [midR(a, 1), midR(a, 3)]);
-end
-hold off;
 end
 
 function [r, c] = splitRC(tri, points)

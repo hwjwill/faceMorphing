aca4bab8004a19061ba46ffa58571ac3c451e2e2
hwjwill <hwjwill@hotmail>
2016-10-20 02:30:07 -0700

    Warp works

--- main.m
+++ main.m
@@ -3,8 +3,14 @@ points1 = csvread(points1csv);
 points2 = csvread(points2csv);
 im1 = imread(imname1);
 im2 = imread(imname2);
-im1 = rgb2gray(im1);
-im2 = rgb2gray(im2);
+% im1 = rgb2gray(im1);
+% im2 = rgb2gray(im2);
+im1r = im1(:, :, 1);
+im1g = im1(:, :, 2);
+im1b = im1(:, :, 3);
+im2r = im2(:, :, 1);
+im2g = im2(:, :, 2);
+im2b = im2(:, :, 3);
 
 points = (points1 + points2) / 2;
 %points = points1 * 0.9 + points2 * 0.1;
@@ -13,12 +19,20 @@ tri = delaunay(points);
 [r1, c1] = splitRC(tri, points1);
 [r2, c2] = splitRC(tri, points2);
 
-result1 = warp(im1, r1, c1, midR, midC);
-result2 = warp(im2, r2, c2, midR, midC);
-figure(1);
-imshow(result1);
-result = result1 * 0.5 + result2 * 0.5;
-figure(2);
+result1r = warp(im1r, r1, c1, midR, midC);
+result2r = warp(im2r, r2, c2, midR, midC);
+resultr = result1r * 0.5 + result2r * 0.5;
+
+result1g = warp(im1g, r1, c1, midR, midC);
+result2g = warp(im2g, r2, c2, midR, midC);
+resultg = result1g * 0.5 + result2g * 0.5;
+
+result1b = warp(im1b, r1, c1, midR, midC);
+result2b = warp(im2b, r2, c2, midR, midC);
+resultb = result1b * 0.5 + result2b * 0.5;
+
+result = cat(3, resultr, resultg, resultb);
+imwrite(result, 'midway.jpg');
 imshow(result);
 % hold on;
 % for a = 1:size(midR, 1)
@@ -41,6 +55,30 @@ for i = 1:size(midR, 1)
         result(y(1, j), y(2, j)) = im1(x(1, j), x(2, j));
     end
 end
+[empR, empC] = find(result == 0);
+for a = 1:size(empR)
+    result(empR(a), empC(a)) = nearestVal(result, empR(a), empC(a));
+end
+end
+
+function [avg] = nearestVal(img, r, c)
+count = 0;
+sum = 0;
+for a = -1:1
+    for b = -1:1
+        candidateR = r + a;
+        candidateC = c + b;
+        if candidateR <= 0 ||  candidateR >= size(img, 1) + 1 ...
+            || candidateC <= 0 || candidateC >= size(img, 2) + 1 ...
+            || img(candidateR, candidateC) == 0
+            continue;
+        else
+            count = count + 1;
+            sum = sum + double(img(candidateR, candidateC));
+        end
+    end
+end
+avg = sum / count;
 end
 
 function [r, c] = splitRC(tri, points)
